58dfab7450462607b016b03b63d384fba28bf2eb
Patel Bilal <bilalpatel78600@gmail.com>
2026-07-16 01:06:05 +0530

    RL example program 1

---
+++ BankAccount.java
@@ -0,0 +1,13 @@
+public class BankAccount{
+    public static void main(String[]args){
+     final String AccountHolder ="Patel Bilal";
+    long AccountNumber = 123456789;
+    java.Math.BigDecimal balance = new java.Math.BigDecimal(3000.00);
+    boolean IsAccountFrozen = false;
+    boolean IsSavingAccount = true;
+    System.out.println("Account Holder : "+ AccountHolder);
+    System.out.println("Has Saving Account: "+ IsSavingAccount);
+    System.out.println("Balance : "+ balance);
+    System.out.println("Account Number :"+ AccountNumber);   
+    }
+}
